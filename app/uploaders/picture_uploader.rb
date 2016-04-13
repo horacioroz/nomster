892b2ef7c5ea981d972d2cc6ce0795ea31012fcf
Horacio Rozanski <horacioroz@gmail.com>
2016-04-13 12:36:38 -0300

    picture_uploder change for test

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -13,11 +13,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    #{Provisto por la gema}"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+   #Lo tenia comentado y en el localhost funcionaba a ver que pasa cuando lo descomento
   end
 
   def extension_whitelist
-    %w(jpg jpeg gif png)
+    %w(jpg jpeg gif png bmp)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
