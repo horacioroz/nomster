752f7b873720d8a58af594906623c4dd7465af53
Horacio Rozanski <horacioroz@gmail.com>
2016-04-21 11:13:53 -0300

    Adding region, host, and endpoint to carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,9 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
     :aws_secret_access_key  => ENV["AWS_SECRET_KEY"],                        # required
-    :region                 => 'sa-east-1'
+    :region                 => 'us-west-2',
+    :host                   => 's3.amazonaws.com',
+    :endpoint               => 'https://s3.amazonaws.com/horacioroznomster/uploads/photo/picture/'
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
