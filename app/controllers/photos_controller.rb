3c51f1379444ad827ce014b772a48a286676bcd7
Horacio Rozanski <horacioroz@gmail.com>
2016-06-14 19:53:59 -0300

    Last did

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,7 +10,7 @@ class PhotosController < ApplicationController
 	private
 
 	def photo_params
-		params.require(:photo).permit(:caption, :picture)
+		params.require(:photo).permit(:caption, :picture, :place_id)
 	end
 
 end
